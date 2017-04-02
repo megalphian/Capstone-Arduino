6d7b1f16312d53ae867b10264cf3ff95997004e6
Megnath Ramesh <megnath@ualberta.ca>
2017-04-01 21:53:10 -0600

    Draft 1 of Wrist controller Arduino script

---
+++ WristController/WristController.ino
@@ -0,0 +1,154 @@
+/*
+ * rosserial Servo Control Example
+ *
+ * This sketch demonstrates the control of hobby R/C servos
+ * using ROS and the arduiono
+ * 
+ * For the full tutorial write up, visit
+ * www.ros.org/wiki/rosserial_arduino_demos
+ *
+ * For more information on the Arduino Servo Library
+ * Checkout :
+ * http://www.arduino.cc/en/Reference/Servo
+ */
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+
+#include <Servo.h> 
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/UInt16MultiArray.h>
+#include <std_msgs/Int16MultiArray.h>
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#define NUM_SERVOS 6  // number of servos that exist in the hand
+#define SERVO_MIN           // minimum angle the servo can reach 
+#define SERVO_MAX           // maximum angle the servo can reach
+#define POT1 A0
+#define POT2 A1
+#define POT3 A2
+#define POT4 A3
+#define POT5 A4
+#define POT6 A5
+#define POT7 A6
+#define POT8 A7
+#define POT9 A8
+#define POT10 A9
+#define FSR1 A10
+#define FSR2 A11
+#define FSR3 A12
+#define FSR4 A13
+#define FSR5 A14
+
+Servo servo[NUM_SERVOS];
+ros::NodeHandle  nh;
+
+//Servo servo;
+std_msgs::UInt16 msg_pub;
+std_msgs::UInt16 msg_pub1;
+
+std_msgs::UInt16MultiArray pot_array_raw; //Raw POT values from ADC 
+//pot_array_raw.layout.dim.push_back(std_msgs::MultiArrayDimension());
+//pot_array_raw.layout.dim[0].size = 9;
+
+std_msgs::Int16MultiArray pot_array;     //Processed POT Angles (in degrees)
+//pot_array.layout.dim.push_back(std_msgs::MultiArrayDimension());
+//pot_array.layout.dim[0].size = 9;
+
+std_msgs::UInt16MultiArray fsr_array_raw;         //Raw FSR values from ADC
+//fsr_array_raw.layout.dim.push_back(std_msgs::MultiArrayDimension());
+//fsr_array_raw.layout.dim[0].size = 5;
+
+std_msgs::Int16MultiArray fsr_array;             //Processed FSR Forces (in grams)
+//fsr_array.layout.dim.push_back(std_msgs::MultiArrayDimension());
+//fsr_array.layout.dim[0].size = 5;
+
+ros::Publisher chatter_pub("servo_pub", &msg_pub);
+ros::Publisher chatter_pub1("sensor_pub", &msg_pub1);
+ros::Publisher pot_pub("potentiometer_raw", &pot_array_raw);
+ros::Publisher pot_proc("potentiometer_processed", &pot_array);
+ros::Publisher fsr_pub("fsr_raw", &fsr_array_raw);
+ros::Publisher fsr_proc("fsr_processed", &fsr_array);
+
+void servo_cb( const std_msgs::UInt16MultiArray& cmd_msg){
+  int i=0;  
+  digitalWrite(13, HIGH-digitalRead(13));  //toggle led
+  for(i=0; i<NUM_SERVOS; i++)
+  {
+    msg_pub.data = cmd_msg.data[i];
+    chatter_pub.publish(&msg_pub);
+    //Serial.print(cmd_msg.data[i]);
+    servo[i].write(cmd_msg.data[i]); //set servo angle, should be from 0-180    
+  } 
+  digitalWrite(13, LOW-digitalRead(13));  //toggle led
+}
+
+ros::Subscriber<std_msgs::UInt16MultiArray> sub("servo", servo_cb);
+
+void setup(){
+  pinMode(13, OUTPUT);
+
+  nh.initNode();
+  nh.subscribe(sub);
+  nh.advertise(chatter_pub);
+  nh.advertise(pot_pub);
+  nh.advertise(pot_proc);
+  nh.advertise(fsr_pub);
+  nh.advertise(fsr_proc);
+
+  servo[0].attach(3);
+  servo[1].attach(4);
+  servo[2].attach(14);
+  servo[3].attach(15);
+  servo[4].attach(16);
+  servo[5].attach(17);
+  //servo.attach(9); //attach it to pin 9
+}
+
+void loop(){
+
+  u16 temp = 0;
+  
+	for(int i = 0; i < 9; i++){
+    	if(temp = analogRead(POT1+i))
+      {}
+      else
+      temp = 0;
+      temp = rand() % 90;
+		pot_array_raw.data[i] = temp;
+    //msg_pub1.data = temp;
+    //chatter_pub1.publish(&msg_pub);		
+		pot_array.data[i] = temp*333.0/1024.0; //Conversion function detailed in Datasheet
+		}
+   pot_pub.publish(&pot_array_raw);
+   pot_proc.publish(&pot_array);
+
+	float fsr_voltage = 0;
+  	for(int i = 0; i < 5; i++){
+    	if(temp = analogRead(FSR1+i))
+    	{}
+    	else
+    	temp = 0;
+      fsr_array_raw.data[i] = temp;
+    	//Convert raw values to forces in grams
+    	fsr_voltage = temp*5.0/1024.0;  //Convert raw value to voltage
+    	fsr_array.data[i] = fsrVolt2Grams(fsr_voltage);  //Convert voltage to Force		
+		}
+   fsr_pub.publish(&fsr_array_raw);
+   fsr_proc.publish(&fsr_array);		
+  nh.spinOnce();
+  delay(1);
+}
+//A function which takes a voltage from an FSR and converts it into the force in grams
+//NOTE: Only works in 1-3.5V range outside this range it will read either 0 or 1128 (the min and max values)
+int fsrVolt2Grams(float voltage){
+  int grams;
+  if(voltage <= 1.0) grams = 0;
+  else if(voltage <= 3.5) grams = (53.2*pow(voltage,4))-(379.2*pow(voltage,3))+(1091.6*pow(voltage,2))-(1288.6*voltage)+541.2; //Conversion function derivation found in companion Excel Sheet
+  else grams = 1128;
+  return grams;
+}
